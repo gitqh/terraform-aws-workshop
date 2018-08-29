f2d103c7d004a8935321f980ea345a4563684495
kobe19881208 <kobe19881208@163.com>
2018-08-29 15:23:19 +0800

    refactor with git release version

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "ec2" {
-  source = "modules/ec2/"
+  source = "github.com/gitqh/terraform-aws-workshop//modules/ec2?ref=v0.0.1"
   ami_id = "ami-08569b978cc4dfa10"
   aws_instance_type = "t2.nano"
   key_name = "quhang"
@@ -9,14 +9,14 @@ module "ec2" {
 }
 
 module "route53" {
-  source = "modules/route53/"
+  source = "github.com/gitqh/terraform-aws-workshop//modules/route53?ref=v0.0.1"
   domain_name = "quhang.workshop.oc-tw.net."
   zone_id = "Z2F25J92GG08RZ"
   records = ["${module.ec2.web-public-ip}"]
 }
 
 module "security" {
-  source = "modules/security/"
+  source = "github.com/gitqh/terraform-aws-workshop//modules/security?ref=v0.0.1"
   http_port = 80
   ssh_port = 22
 }
