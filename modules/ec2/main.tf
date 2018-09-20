bc6a2903ae421386defaab00353b9c6981f89918
kobe19881208 <kobe19881208@163.com>
2018-09-20 10:23:55 +0800

    add tls module

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -6,7 +6,8 @@ resource "aws_instance" "server" {
   provisioner "file" {
     connection {
       user = "ec2-user"
-      private_key = "${file(var.key_path)}"
+      type        = "ssh"
+      private_key = "${tls_private_key.bootstrap_private_key.private_key_pem}"
     }
     source      = "./html"
     destination = "/tmp"
@@ -24,4 +25,14 @@ resource "aws_instance" "server" {
     ]
   }
   tags = "${map("Name", "${var.tag_name}")}"
+}
+
+resource "tls_private_key" "bootstrap_private_key" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+resource "aws_key_pair" "bootstrap_generated_key" {
+  key_name   = "${var.key_name}"
+  public_key = "${tls_private_key.bootstrap_private_key.public_key_openssh}"
 }
